07d71be9fb6a31164b1e0d4973c4b83d49e87d5e
Fernando Saraiva <fpsaraiva@gmail.com>
2021-12-09 12:23:58 -0300

    ajustes no formulario de cadastro

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,9 +12,9 @@ export class LoginComponent {
 
   username: string;
   password: string;
-  loginError: boolean;
   cadastrando: boolean;
   mensagemSucesso: string | null;
+  errors: String[];
 
   constructor(
     private router: Router,
@@ -42,10 +42,13 @@ export class LoginComponent {
       .salvar(usuario)
       .subscribe( response => {
         this.mensagemSucesso = "Cadastro realizado com sucesso! Efetue o login.";
-        this.loginError = false;
-      }, error => {
-        this.loginError = true;
+        this.cadastrando = false;
+        this.username = '';
+        this.password = '';
+        this.errors = [];
+      }, errorResponse => {
         this.mensagemSucesso = null;
+        this.errors = errorResponse.error.mensagens;
       })
   }
 }
